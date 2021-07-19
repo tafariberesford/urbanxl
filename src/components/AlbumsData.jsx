4f2a4c19e5ef91684c9f4d4d153134956bb6c418
Tafari Beresford <tafariberesford@Tafaris-MBP.fios-router.home>
2021-07-19 15:20:33 -0400

    added init comments

--- src/components/AlbumsData.jsx
+++ src/components/AlbumsData.jsx
@@ -12,13 +12,25 @@ const headers = {Authorization: `Bearer ${AIRTABLE_KEY}` }
 
 export default function AlbumsData() {
   const {id} = useParams();
-  const [ album, setAlbum ] = useState({});
+  const [album, setAlbum] = useState({});
+  const [replies, setReplies] = useState([]);
 
   useEffect(() => {
+    const fetchReplies = async (repliesArray) => {
+      let repliesURL = `https://api.airtable.com/v0/appBlbkoSrRUxh4cI/replies/`
+      repliesArray.forEach(async(reply) => {
+        const response = await axios.get(`${repliesURL}${reply}`, { headers })
+        setReplies((prev) => ([... prev, response.data]))
+      })
+    }
+
     const fetchAlbum = async () => {
       const URL = `${AIRTABLE_URL}/${id}`;
       const response = await axios.get(URL, { headers, })
       setAlbum(response.data);
+      if (response.data.fields?.replies) {
+        fetchReplies(response.data.fields?.replies)
+      }
     };
     fetchAlbum();
   }, [id]);
@@ -28,6 +40,11 @@ export default function AlbumsData() {
       <img src={album.fields?.imgURL} />
       <p>{album.fields?.releaseDate}</p>
       <p>{album.fields?.review}</p>{"\n"}
+      <div className="replies">
+        {replies.map((reply) => {
+          return <h1>{reply.fields?.reply}</h1>
+        })}
+      </div>
     </div>
   )
 }
---
+++ src/components/Replies.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+import { useState } from "react";
+import {Button, Input} from "antd"
