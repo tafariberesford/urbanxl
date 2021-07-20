da90cf3a76625a308929d66e44bf6ee345c70ee0
Tafari Beresford <tafariberesford@Tafaris-MBP.fios-router.home>
2021-07-20 10:09:59 -0400

    modified css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Tafari Beresford 2021 Footer</div>;
+  return <div>UrbanXL 2021</div>;
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>
-      <Link to="/"><h1>Urban XL</h1></Link>
-      <Link to="/urban-albums">Top 10 Albums</Link>
+    <div className="navbar">
+      <Link to="/"><h1 className="urban">Urban XL</h1></Link>
+      <Link to="/urban-albums"><h2 className="urban">Top 10 Albums</h2></Link>
     </div>
   );
 }
